ba8b1d7598fc2b7eb3d72fb6ccc8804c4c90c09e
Michael Wang <wang4860@umn.edu>
2017-12-02 17:02:25 -0600

    translation!

--- mobile/screens/CameraDemoScreen.js
+++ mobile/screens/CameraDemoScreen.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Text, Button, StyleSheet, Image, View, Picker } from 'react-native';
 import { ImagePicker } from 'expo';
-
-//import vision from "react-cloud-vision-api";
-//import vision from "@google-cloud/vision";
-//const client = vision.ImageAnnotatorClient();
-
 class CameraDemoScreen extends React.Component {
   state = {
     image_uri: null,
     image_base64: null,
     API_KEY: "dd154ce6f099dcdcf45319997621dc13601acf73",
     vision_req: null,
-
     english_word: "",
     dst_lang: "",
+    translation: "",
     final_uri: null
   };
+
+  // <Button
+  //   onPress={() => this.props.navigation.navigate('Translation')}
+  //   title="Translate Image"
+  //   style={styles.spaced}
+  // />
 
   render() {
     let { image_uri } = this.state;
@@ -25,23 +26,29 @@ class CameraDemoScreen extends React.Component {
       ConditionalRender = (<View>
                 <Image source={{uri: image_uri}} style={{ width: 300, height: 300, marginTop: 20 }} />
                 <Text> {this.state.english_word} </Text>
-                <View>
-        <Picker
-          selectedValue={this.state.dst_lang}
-          onValueChange={dst_lang => this.setState({ dst_lang })}
-          style={{ width: 160 }}
-          mode="dropdown">
-
-          <Picker.Item label="Chinese-Simplified" value="zh-CN"/>
-          <Picker.Item label="Spanish" value="es"/>
-          <Picker.Item label="Hindi" value="hi"/>
-        </Picker>
-      </View>
-                <Button
-                  onPress={() => this.props.navigation.navigate('Translation')}
-                  title="Translate Image"
-                  style={styles.spaced}
-                />
+                <Picker
+                  selectedValue={this.state.dst_lang}
+                  onValueChange={dst_lang => {
+                    let base_url = 'https://translation.googleapis.com/language/translate/v2';
+                    let google_url = base_url + "?q=" + encodeURI(this.state.english_word) + "&target=" + dst_lang + "&source=en&key=AIzaSyAllxK-KhFvNMtTqkA59tfUkQCGAYNHi5I";
+                    fetch(google_url, {
+                      method:"POST"})
+                      .then(res => res.json())
+                      .then(json => {
+                        console.log(json.data.translations[0].translatedText);
+                        this.setState({translation: json.data.translations[0].translatedText, dst_lang: dst_lang});
+                      })
+                      .catch(err => {
+                        console.log(err);
+                      });
+                  }}
+                  style={{ width: 160 }}
+                  mode="dropdown">
+                  <Picker.Item label="Chinese-Simplified" value="zh-CN"/>
+                  <Picker.Item label="Spanish" value="es"/>
+                  <Picker.Item label="Hindi" value="hi"/>
+                </Picker>
+                <Text> {this.state.translation} </Text>
                 </View>);
     } else {
     ConditionalRender = (<View>
@@ -64,77 +71,34 @@ class CameraDemoScreen extends React.Component {
       aspect: [4, 3],
       base64:true
     });
-
-    // console.log(result);
-
     if (!result.cancelled) {
       this.setState({ image_base64: result.base64 });
       this.setState({image_uri: result.uri});
-
-//vision.init({auth: this.state.API_KEY})
-const new_req = {
-  "requests": [
-    {
-      "image": {
-        "content": this.state.image_base64
-      },
-      "features": [
-        {
-          "type": "LABEL_DETECTION",
-          "maxResults": 5
-        }
-      ]
-    }
-  ]
-};
-const vision_uri = "https://vision.googleapis.com/v1/images:label?key=dd154ce6f099dcdcf45319997621dc13601acf73";
-
-var temp = encodeURIComponent(JSON.stringify(new_req));
-var x = vision_uri + temp;
-
-fetch("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyC21ZnfUsGG1g5RGNNwWA6-6_tVfXu96sg", {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(new_req)
-})
-.then(res => JSON.parse(res._bodyInit))
-.then(body => {
-
-  // console.log(body.responses[0].labelAnnotations[0].description);
-  this.setState({english_word : body.responses[0].labelAnnotations[0].description});
-});
-  //  {
-  // // console.log(res);
-  // // console.log(res["_bodyInit"]);
-  // // console.log(res._bodyInit);
-  // console.log(res._bodyInit.parse);
-
-// });
-// client.labelDetection(this.state.image_base64)
-// .then(results => {
-//   const labels = results[0].labelAnnotations;
-//
-//   console.log('Labels:');
-//   labels.forEach(label => console.log(label.description));
-// })
-
-// const req = new vision.Request({
-//   image: new vision.Image({
-//     base64: this.state.image_base64,
-//   }),
-//   features: [
-//     new vision.Feature('LABEL_DETECTION', 1)
-//   ]
-// })
-// console.log("this is the req");
-// console.log(req);
-// console.log("after req");
-//this.setState(vision_req: req);
-
-      // this is where we call google api
+      const new_req = {
+        "requests": [{
+          "image": {
+            "content": this.state.image_base64 },
+            "features": [{
+                "type": "LABEL_DETECTION",
+                "maxResults": 5
+            }]
+        }]
+      };
+      const vision_uri = "https://vision.googleapis.com/v1/images:label?key=dd154ce6f099dcdcf45319997621dc13601acf73";
+      var temp = encodeURIComponent(JSON.stringify(new_req));
+      var x = vision_uri + temp;
+      fetch("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyC21ZnfUsGG1g5RGNNwWA6-6_tVfXu96sg", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(new_req)
+      })
+      .then(res => JSON.parse(res._bodyInit))
+      .then(body => {
+        this.setState({english_word : body.responses[0].labelAnnotations[0].description});
+      });
     }
   };
 }
